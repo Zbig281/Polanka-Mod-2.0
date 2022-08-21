df9a2aeb49bea1d3d3c99c81d54d853a91811172
Zbig <zbig.281@googlemail.com>
2022-08-21 16:01:22 +0200

    add Chieftain Sword

--- Server/sql/dump.sql
+++ Server/sql/dump.sql
@@ -1952,7 +1952,8 @@ INSERT INTO `objects_types` (`ID`, `ParentID`, `Name`, `IsContainer`, `IsMovable
 (3007,59,'Tribe Mole Carcass',1,1,0,0,0,0,0,100000,5,0,1,'art\\images\\universal',0,0,0,0,0,0,'','',0,300,0,0),
 (3008,751,'Tribe Witch',0,1,0,0,0,0,0,0,0,0,1,'',0,0,0,0,0,0,'','',0,NULL,0,0),
 (3009,59,'Tribe Witch Carcass',1,1,0,0,0,0,0,100000,5,0,1,'art\\images\\universal',0,0,0,0,0,0,'','',0,300,0,0),
-(3010,191,'Bear Axe',0,0,0,0,0,0,0,0,4,1,3200,'',0,0,0,0,0,0,'art\\2D\\Items\\bearaxe.png','',0,NULL,1,1);
+(3010,191,'Bear Axe',0,0,0,0,0,0,0,0,4,1,3200,'',0,0,0,0,0,0,'art\\2D\\Items\\bearaxe.png','',0,NULL,1,1),
+(3011,57,'Chieftain Sword',0,0,0,0,0,0,0,0,4,1,4500,'',0,0,0,0,0,0,'art\\2D\\Items\\chieftainsword.png','',3000,NULL,1,1);
 --
 -- Dumping data for table `objects_conversions`
 --
