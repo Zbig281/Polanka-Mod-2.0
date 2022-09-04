a948036c88e73d890ca984cfd2ee5926aa84c2f0
Zbig <zbig.281@googlemail.com>
2022-09-04 02:38:24 +0200

    update

--- Server/TTmod/config.cs
+++ Server/TTmod/config.cs
@@ -2,8 +2,8 @@
 $NyuRCON::Interval = 20;
 
 // MOTD Configuration (Message of the day)
-$TTmod::EnableMOTD = false;
-$TTmod::MOTD = "Welcome to my awesome server, {CharName}!";
+$TTmod::EnableMOTD = true;
+$TTmod::MOTD = "Witamy na Polance 2.0, {CharName}! Zapraszamy na LiveMape:https://livemap.feudal.tools/?livemap_id=9944";
 
 // Inform players about their playtime in system chat after login
 $TTmod::PlaytimeInfo = true;
@@ -11,7 +11,7 @@ $TTmod::PlaytimeInfo = true;
 // GuildGUI Configuration
 // Check the 'Information' tab on your Livemap configuration page to aquire your GuildGUI Address 
 $GuildGUI::Enable = false;
-$GuildGUI::Address = "http://91.2.48.87:8080/index.php?livemap_id=1&action=GuildGUI";
+$GuildGUI::Address = "https://livemap.feudal.tools/index.php?livemap_id=9944&action=GuildGUI";
 
 // Debug Mode - Write extensive output to server console
 $TTmod::Debug = false;
