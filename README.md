b998e726e0f7b21adf6f55102164e4b425ceb57e
Zbig <zbig.281@googlemail.com>
2022-10-21 08:34:59 +0200

    update

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ All finish for work server.
 update folder is for client send basilMod.
 
 For the Knole trigger to drop their items, you need MariaDB v10.
+
+Auto restart with in-game information about the restart and the countdown time.
+ServerRestart/Restart.cs
+
+in check_server.cmd you have to change the server ID as it is entered in config / world_1.xml for example.
+- world_1.xml
+<ID>1</ID>, <ID>2</ID>, <ID>3</ID>
+
+- check_server.cmd
+WorldID %1 , WorldID %2 , WorldID %3
--- Server/check_server.cmd
+++ Server/check_server.cmd
@@ -5,17 +5,17 @@ REM Delay until server process is closed...
 :WAITCLOSE
 <nul (set/p _any_variable=.)
 ping ::1 -n 3 >NUL 2>NUL
-tasklist /FI "imagename eq ddctd_cm_yo_serve.exe" /V | find "world ID %4" >NUL && goto :WAITCLOSE
+tasklist /FI "imagename eq ddctd_cm_yo_serve.exe" /V | find "world ID %1" >NUL && goto :WAITCLOSE
 echo done.
 ping ::1 -n 5 >NUL 2>NUL
 
 :LOOP
 REM If server is started, exit batch file.
-tasklist /FI "imagename eq ddctd_cm_yo_serve.exe" /V | find "world ID %4" >NUL && goto :END
+tasklist /FI "imagename eq ddctd_cm_yo_serve.exe" /V | find "world ID %1" >NUL && goto :END
 
 REM If not, try to start it.
-echo Launching WorldID %4.
-start ddctd_cm_yo_serve.exe -WorldID %4
+echo Launching WorldID %1.
+start ddctd_cm_yo_serve.exe -WorldID %1
 REM Wait 30 seconds before next check to give server time to initialize titlebar with world ID.
 echo Waiting 30 seconds to ensure server has started.
 PING ::1 -n 30 >NUL 2>NUL
