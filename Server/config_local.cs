e8582385b583f8b32435c9f7637772131f0b3f3d
Zbig <zbig.281@googlemail.com>
2022-10-21 08:30:17 +0200

    update

--- Server/config_local.cs
+++ Server/config_local.cs
@@ -1,3 +1,3 @@
 $cm_config::DB::Connect::server =   "127.0.0.1"; // server IP or domain name. Can enter port if it is not default
 $cm_config::DB::Connect::user =     "root"; // MUST be a user with ROOT privileges in order to create new DBs
-$cm_config::DB::Connect::password = "Zbigniew281"; // password for that user
+$cm_config::DB::Connect::password = "yourpw"; // password for that user
